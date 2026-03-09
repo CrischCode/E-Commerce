e967c9dc1755eecfd24ac463f384a9384e2b323b
CrischCode <cchamomorales@gmail.com>
2026-03-08 20:50:51 -0600

    Modificacion a DTOs Pedidos
    Modificacion para la direccion de envio

--- Ecommerce.Shared/DTOs/PedidoDtos.cs
+++ Ecommerce.Shared/DTOs/PedidoDtos.cs
@@ -6,43 +6,47 @@ using System.Threading.Tasks;
 
 namespace Ecommerce.Shared.DTOs
 {
-    public class PedidoReadDto
-    {
-      public int IdPedido {get; set;}
-      public int IdCliente {get; set;}
-      public string Cliente {get; set;} = string.Empty;
-      public int IdMetodoPago {get; set;}
-      public string? MetodoPago {get; set;} = string.Empty;
-      public DateOnly FechaPedido{get; set;}
-      public decimal Total {get; set;}
-      public string? Estado {get; set;}
-      //public decimal? SubTotal {get; set;} 
+  public class PedidoReadDto
+  {
+    public int IdPedido { get; set; }
+    public int IdCliente { get; set; }
+    public string Cliente { get; set; } = string.Empty;
+    public int IdMetodoPago { get; set; }
+    public string? MetodoPago { get; set; } = string.Empty;
+    public DateOnly FechaPedido { get; set; }
+    public decimal Total { get; set; }
+    public string? Estado { get; set; }
+    //public decimal? SubTotal {get; set;} 
 
-      public List<DetallePedidoReadDto> Detalles { get; set; } = new();
-    }
+    public List<DetallePedidoReadDto> Detalles { get; set; } = new();
+  }
 
-    public class PedidoCreateDto
-    {
-      public int IdCliente {get; set;}
-      public int IdMetodoPago {get; set;}
+  public class PedidoCreateDto
+  {
+    public int IdCliente { get; set; }
+    public int IdMetodoPago { get; set; }
+    public string DireccionEnvio { get; set; } = null!;
+    public string CiudadEnvio { get; set; } = null!;
+    public string PaisEnvio { get; set; } = null!;
+    public string CodigoPostalEnvio { get; set; } = null!;
 
-      [Required]
-      [MinLength(1, ErrorMessage = "El pedido debe de tener almenos un producto.")]
-     public List<DetallePedidoCreateDto> Detalles {get; set;} = null!;
-    }
+    [Required]
+    [MinLength(1, ErrorMessage = "El pedido debe de tener almenos un producto.")]
+    public List<DetallePedidoCreateDto> Detalles { get; set; } = null!;
+  }
 
-    public class PedidoUpdateDto
-    {
-      public int? IdMetodoPago {get; set;}
-      public string? Estado {get; set;} 
+  public class PedidoUpdateDto
+  {
+    public int? IdMetodoPago { get; set; }
+    public string? Estado { get; set; }
 
-     // public List<DetallePedidoReadDto> Detalle {get; set;} = new();
-    }
+    // public List<DetallePedidoReadDto> Detalle {get; set;} = new();
+  }
 
-    //Paginado de Pedidos
-     public class PedidoResponse<T>
-        {
-            public int Total {get; set;}
-            public List<PedidoReadDto> Date {get; set;} = new();
-        } 
+  //Paginado de Pedidos
+  public class PedidoResponse<T>
+  {
+    public int Total { get; set; }
+    public List<PedidoReadDto> Date { get; set; } = new();
+  }
 }
